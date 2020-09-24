173acc8634c0ca7a864210bea149ac89458ba0ea
Rikuto Yamaguchi <yamaguchirkt@gmail.com>
2020-09-24 17:55:44 +0900

    Update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-  ssr: false,
+  ssr: true,
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
@@ -36,4 +36,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+  generate: {
+    dir: 'functions/dist',
+  },
 }
